61099b3e903320e3de4175cf72a32ced8644d427
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-07-04 12:22:43 -0400

    connect db

--- src/server.js
+++ src/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
+import 'dotenv/config'
+import mongoose from 'mongoose';
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+mongoose.Promise = global.Promise
 
+const DB_URL = process.env.MONGO_URL;
+
+// Connect to mongoose
+mongoose.connect(DB_URL, { useNewUrlParser: true }, (err) => {
+    if (err)
+        return console.log('Unable to Connect to MongoDB')
+    return console.log('Connection Successful')
+})
 
 
 app.listen(port, () => console.log('server is running'));
