f1822e591ba73eea0ebbe4fe277f4c76c6ccfe92
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-07-04 16:06:45 -0400

    add topic model

---
+++ src/models/topic.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const isValidTopic = (topic) => {
+    const regExp = /^([A-Za-z]+[,.]?[ ]?|[A-Za-z]+['-]?)+$/i
+    return regExp.test(topic)
+  };
+  
+  const topicSchema = mongoose.Schema({
+    
+    _id: mongoose.Schema.Types.ObjectId,
+    topic: {
+        type: String, 
+        required: [true, 'topic is required'], 
+        min:2, 
+        max: 12,
+        validate: [isValidTopic, 'Please enter valid topic'],
+    },
+});
+
+
+
+export default mongoose.model('Topic', TopicSchema);
